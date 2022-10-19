6a71775e8fc93e264281ac7e40325c89b0937d1f
BerdyshevEugene <berdyshevevg@mail.ru>
2022-10-19 22:13:30 +0800

    2-nd commit

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -124,8 +124,8 @@ DJOSER = {
     },
 
     'PERMISSIONS': {
-        'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
-        'user_list': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
+        'user_list': ('rest_framework.permissions.AllowAny',),
+        'user': ('rest_framework.permissions.AllowAny',),
     },
     'HIDE_USERS': False,
 }

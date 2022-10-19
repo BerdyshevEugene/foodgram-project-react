926c352fbe2e27dbfdcaabe4f0e5bb7e1e24d57f
BerdyshevEugene <berdyshevevg@mail.ru>
2022-10-19 22:23:47 +0800

    2-nd commit

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -103,14 +103,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'users.User'
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     ],
 
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    ],
+    ),
 
     'DEFAULT_PAGINATION_CLASS': 'api.paginations.PageNumberPagination',
     'PAGE_SIZE': 6

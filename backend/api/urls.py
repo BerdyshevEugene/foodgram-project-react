8cad0ce32945be294adccde03303c84739db54bd
BerdyshevEugene <berdyshevevg@mail.ru>
2022-10-21 22:54:34 +0800

    2-nd commit

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -18,7 +18,8 @@ router.register('recipes', RecipeViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/token/login/', views.TokenCreateView.as_view(), name='login'),
-    path('auth/token/logout/', views.TokenDestroyView.as_view(), name='logout'),
+    path('auth/token/logout/', views.TokenDestroyView.as_view(),
+         name='logout'),
     path(
         'recipes/download_shopping_cart/',
         download_shopping_cart,

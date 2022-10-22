f58f44e811f4e669d66acedba86aebcba57c5acc
BerdyshevEugene <berdyshevevg@mail.ru>
2022-10-22 17:19:04 +0800

    2-nd commit

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -14,10 +14,6 @@ router.register(r'recipes', RecipeViewSet)
 router.register(r'tags', TagViewSet)
 
 urlpatterns = [
-#     path('users/<int:id>/subscribe/', SubscribeApiView.as_view(),
-#          name='subscribe'),
-#     path('users/subscriptions/', ListSubscribeViewSet.as_view(),
-#          name='subscriptions'),
     path('', include(router.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),

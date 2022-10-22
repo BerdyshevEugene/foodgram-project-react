e78617f5e596fbdde610a09e5f7fe6b187cad25e
BerdyshevEugene <berdyshevevg@mail.ru>
2022-10-22 14:22:24 +0800

    2-nd commit

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,12 +8,26 @@ app_name = 'api'
 
 router = DefaultRouter()
 
-router.register('users', CustomUserViewSet)
-router.register('tags', TagViewSet)
-router.register('ingredients', IngredientViewSet)
-router.register('recipes', RecipeViewSet)
+router.register(r'users', CustomUserViewSet)
+router.register(r'tags', TagViewSet)
+router.register(r'ingredients', IngredientViewSet)
+router.register(r'recipes', RecipeViewSet)
 
 urlpatterns = [
+     path(
+        r'recipes/<recipe_id>/favorite/',
+        RecipeViewSet.as_view({
+            'post': 'create',
+            'delete': 'delete'
+        }),
+        name='favorite'),
+    path(
+        r'recipes/<recipe_id>/shopping_cart/',
+        RecipeViewSet.as_view({
+            'post': 'create',
+            'delete': 'delete'
+        }),
+        name='shopping_cart'),
     path('', include(router.urls)),
     path('users/<int:id>/subscribe/', SubscribeApiView.as_view(),
          name='subscribe'),
